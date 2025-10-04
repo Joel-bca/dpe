1390376b40ee5040547a2ac345f8b3b9924df7bd
Joel152 <135132469+Joel152@users.noreply.github.com>
2025-10-05 02:21:18 +0530

    Remove node_modules and .next from repr

--- .next/static/chunks/src_app_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e5ed4367._.js",
-  "static/chunks/src_bd839b49._.js"
-],
-    source: "dynamic"
-});
